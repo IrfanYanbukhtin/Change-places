e83ffa62cd0267a00a991551712d87661b2fab1b
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-29 21:48:47 +0400

    change places

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Lengh of Massive:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxIndex = 0;
+            int minIndex = 0;
+
+            for (int i = 1 ; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[maxIndex])
+                    maxIndex = i;
+                if (arr[i] < arr[minIndex])
+                    minIndex = i;
+            }
+            int tmp = arr[minIndex];
+            arr[minIndex] = arr[maxIndex];
+            arr[maxIndex] = tmp;
+
+            for (int i = 0; i < arr.Length; i++)
+                {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
